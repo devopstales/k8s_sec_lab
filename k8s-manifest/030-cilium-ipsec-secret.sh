87c19a5a17063bafa63910c3a7bc6fe278b044b0
devopstales <42894256+devopstales@users.noreply.github.com>
2021-10-25 13:45:52 +0200

    mod nginx service

---
+++ k8s-manifest/030-cilium-ipsec-secret.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+kubectl create -n kube-system secret generic cilium-ipsec-keys \
+    --from-literal=keys="3 rfc4106(gcm(aes)) $(echo $(dd if=/dev/urandom count=20 bs=1 2> /dev/null | xxd -p -c 64)) 128"
+
