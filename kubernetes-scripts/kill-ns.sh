77b5a34b568f6ca4d2f098d4bd94ebeebdcdb387
devopstales <42894256+devopstales@users.noreply.github.com>
2023-07-12 08:58:14 +0200

    Update and rename deletenamepsace.sh to kill-ns.sh

--- kubernetes-scripts/kill-ns.sh
+++ kubernetes-scripts/kill-ns.sh
@@ -18,7 +18,7 @@ set -eo pipefail
 die() { echo "$*" 1>&2 ; exit 1; }
 
 need() {
-    which "$1" &>/dev/null || die "Binary '$1' is missing but required"
+	which "$1" &>/dev/null || die "Binary '$1' is missing but required"
 }
 
 # checking pre-reqs
@@ -35,7 +35,7 @@ test -n "$PROJECT" || die "Missing arguments: kill-ns <namespace>"
 kubectl proxy &>/dev/null &
 PROXY_PID=$!
 killproxy () {
-    kill $PROXY_PID
+	kill $PROXY_PID
 }
 trap killproxy EXIT
 
@@ -44,5 +44,3 @@ sleep 1 # give the proxy a second
 kubectl get namespace "$PROJECT" -o json | jq 'del(.spec.finalizers[] | select("kubernetes"))' | curl -s -k -H "Content-Type: application/json" -X PUT -o /dev/null --data-binary @- http://localhost:8001/api/v1/namespaces/$PROJECT/finalize && echo "Killed namespace: $PROJECT"
 
 # proxy will get killed by the trap
-
-kubectl patch namespace cattle-system -p '{"metadata":{"finalizers":[]}}' --type='merge' -n "$PROJECT"
